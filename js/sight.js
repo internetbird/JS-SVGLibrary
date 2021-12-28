84325b1976a2f071f8361acd6af52cff00bd552c
Shai Aharoni <saharoni@gmail.com>
2021-12-28 08:29:25 +0200

    All tests passing

--- js/sight.js
+++ js/sight.js
@@ -26,8 +26,11 @@ class SVGElement {
 
 class Sight {
     constructor(selector, width, height){
-        this.svg = new SVGElement('svg')
-        .attr({viewBox: `0 0 ${width} ${height}`})
-        .append(selector);
+        this.svg = new SVGElement('svg').attr({viewbox: `0 0 ${width} ${height}`}).append(selector);
+
+    }
+
+    draw(type, attrs) {
+        return new SVGElement(type).attr(attrs).append(this.svg);
     }
 }
